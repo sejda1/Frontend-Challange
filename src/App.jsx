3c75396e01bfec9017ba6693a7de2abcb3ab0d4d
sejda <sejdaozgur@gmail.com>
2024-12-28 00:39:14 +0300

    Komponentler olusturuldu.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Portfolyo from "./pages/Portfolyo";
+
 export default function App() {
   return (
-    <h1 className="text-3xl bg-red-500 font-bold underline">
-      Hello world!
-    </h1>
+   <>
+   <Portfolyo/>
+   </>
   )
 }
---
+++ src/pages/Portfolyo.jsx
@@ -0,0 +1,22 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Profile from "../components/Profile";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+
+
+
+
+export default function Portfolyo () {
+    return (
+        <>
+        <Header/>
+        <Hero/>
+        <Skills/>
+        <Profile/>
+        <Projects/>
+        <Footer/>
+        </>
+    )
+}
