798afc990ab85647d5a5999a7107e0085c25b751
sejda <sejdaozgur@gmail.com>
2024-12-29 01:50:59 +0300

    data olusturuldu

--- src/contexts/DarkModeContext.jsx
+++ src/contexts/DarkModeContext.jsx
@@ -1,22 +1,21 @@
-import { createContext, useCallback, useContext} from 'react';
+import { createContext, useContext } from 'react';
 import useLocalStorage from '../hooks/useLocalStorage';
 
 const DarkModeContext = createContext();
 
-export default function DarkModeProvider({children}) {
-    const [darkMode, setDarkMode] = useLocalStorage('darkMode', false);
+export default function DarkModeProvider({ children }) {
+  const [darkMode, setDarkMode] = useLocalStorage('darkMode', false);
 
-    const toggleDarkMode = useCallback(() => {
-        setDarkMode((prevMode) => !prevMode);
-    }, [setDarkMode]);
-
-    return (
-        <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
-            {children}
-        </DarkModeContext.Provider>
-    );
-}
-export const useDarkMode = () => {
-    return useContext(DarkModeContext);
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
   };
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
 
+export const useDarkMode = () => {
+  return useContext(DarkModeContext);
+};
