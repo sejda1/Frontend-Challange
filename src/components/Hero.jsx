7fe27d0e79baf70109a209bc2ce2ed90971bd6f0
sejda <sejdaozgur@gmail.com>
2024-12-30 10:38:45 +0300

    datadan veriler dogru sekilde cekildi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,74 +1,59 @@
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { useLanguage } from "../contexts/LanguageContext";
 import { useDarkMode } from "../contexts/DarkModeContext";
-import dataEng from "../data/dataEng.json";
-import dataTr from "../data/dataTr.json";
 import heroImage from "../../public/assets/sej-vesikalik.jpeg";
 
 function Hero() {
-  const { language } = useLanguage(); 
-  const { darkMode } = useDarkMode(); 
-
-  // Dil bilgisine göre doğru veriyi seç
-  const data = language === "dataTr" ? dataTr : dataEng;
-  console.log(data)
-
-  const { heroTitle, heroDesc, githubLink, linkedinLink } = data.hero;
+  const { toggleLanguage } = useLanguage();
+  const { darkMode } = useDarkMode();
 
   return (
     <section
-    className="p-8 md:p-16 pt-32 flex  min-h-screen "
-    //https://encycolorpedia.com.tr/
-    style={
-        darkMode
-          ? {
-              background: "rgb(23,16,67)",
-              backgroundImage:
-                "linear-gradient(90deg, rgba(22, 15, 36, 1.0) 67%, rgba(26,33,11,1) 67%)",
-            }
-          : {
-              background: "rgb(71,49,211)",
-              backgroundImage:
-                "linear-gradient(90deg, rgba(71,49,211,1) 67%, rgba(107, 142, 35, 1.0) 67%)",
-            }
-      }
-  >
-    <div className="flex flex-col  w-full text-white gap-8 md:px-20">
-      <p className="font-[700] text-[32px] text-[#CBF281]">Sejda ÖZGÜR FIÇICI</p>
-      <div className="flex h-full items-center text-start  xl:flex-row flex-col">
-        <div className="w-full xl:w-3/5  py-4  flex flex-col  ">
-          <p className="text-[44px] w-3/4 font-[700] text-[#CBF281]">
-            {heroTitle}
-          </p>
-          <p className="mt-4 text-[20px] font-[400]">
-            {heroDesc}
-          </p>
-          <div className="mt-8 flex flex-col md:flex-row gap-4">
-            <a
-              href={githubLink}
-              className="bg-white flex items-center gap-2 text-black py-3 px-4 rounded hover:bg-black hover:text-white"
-            >
-              <FaGithub size={25} /> Github
-            </a>
-            <a
-              href={linkedinLink}
-              className="bg-white flex items-center gap-2 text-black py-3 px-4 rounded hover:bg-blue-600 hover:text-white"
-            >
-              <FaLinkedin size={25} /> Linkedin
-            </a>
+      className="p-8 md:p-16 pt-32 flex min-h-screen"
+      style={{
+        backgroundImage: darkMode
+          ? "linear-gradient(90deg, rgba(22,15,36,1) 67%, rgba(26,33,11,1) 67%)"
+          : "linear-gradient(90deg, rgba(71,49,211,1) 67%, rgba(203,242,129,1) 67%)",
+      }}
+    >
+      <div className="flex flex-col w-full text-white gap-8 md:px-20">
+        <p className="font-[700] text-[32px] text-[#CBF281]">
+          Sejda ÖZGÜR FIÇICI
+        </p>
+        <div className="flex h-full items-center text-start xl:flex-row flex-col">
+          <div className="w-full xl:w-3/5 py-4 flex flex-col">
+            <p className="text-[44px] w-3/4 font-[700] text-[#CBF281]">
+              {toggleLanguage("hero").heroTitle}
+            </p>
+            <p className="mt-4 text-[20px] font-[400]">
+              {toggleLanguage("hero").heroDesc}
+            </p>
+            <div className="mt-8 flex flex-col md:flex-row gap-4">
+              <a
+                href={toggleLanguage("githubLink")}
+                className="bg-white flex items-center gap-2 text-black py-3 px-4 rounded hover:bg-black hover:text-white"
+              >
+                <FaGithub size={25} /> Github
+              </a>
+              <a
+                href={toggleLanguage("linkedinLink")}
+                className="bg-white flex items-center gap-2 text-black py-3 px-4 rounded hover:bg-blue-600 hover:text-white"
+              >
+                <FaLinkedin size={25} /> Linkedin
+              </a>
+            </div>
+          </div>
+
+          <div className="w-full md:w-2/5 px-4 flex items-start justify-center md:justify-start">
+            <img
+              src={heroImage}
+              alt="Frontend Developer"
+              className="rounded-lg shadow-xl w-full max-w-[300px] min-w-[300px] object-cover"
+            />
           </div>
         </div>
-
-        <div className="w-full md:w-2/5 px-4  flex items-start justify-center md:justify-start">
-          <img
-            src={heroImage}
-            alt="FrontendDev"
-            className="rounded-lg shadow-xl w-full max-w-[300px] min-w-[300px] object-cover"
-          />
-        </div>
       </div>
-    </div>
-  </section>
+    </section>
   );
 }
 
--- src/contexts/LanguageContext.jsx
+++ src/contexts/LanguageContext.jsx
@@ -1,14 +1,19 @@
-import { createContext, useState, useContext } from 'react';
+import { createContext, useContext } from 'react';
+import useLocaleStorage from "../hooks/useLocalStorage";
+import EN_LANG from "../data/dataEng.json";
+import TR_LANG from "../data/dataTr.json";
 
 export const LanguageContext = createContext();
 
 export function LanguageProvider({ children }) {
-  const [language, setLanguage] = useState('eng'); 
+  const [language, setLanguage] = useLocaleStorage("language",'tr'); 
 
 
-  const toggleLanguage = () => {
-    setLanguage((prevLanguage) => (prevLanguage === 'tr' ? 'eng' : 'tr'));
+  const toggleLanguage = (key) => {
+    const translate = language === "tr" ? TR_LANG : EN_LANG; 
+    return translate[key] || key; 
   };
+  
 
   return (
     <LanguageContext.Provider value={{ language, setLanguage, toggleLanguage }}>
